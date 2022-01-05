33beefe19987d2a84e90e506264a157760a6933c
Anton Zarembo <anton.zarembo@onilab.com>
2022-01-05 10:42:31 +0300

    refactor(cli): changed directories structure

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs/yargs')
+const { join } = require('path')
 const { hideBin } = require('yargs/helpers')
 
 
 yargs(hideBin(process.argv))
-	.commandDir('../commands')
+	.commandDir(join(__dirname, 'lib', 'commands'))
 	.demandCommand()
 	.help()
 	.argv

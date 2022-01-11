dc4a2b7d397633b669710e24ac13eb550ca409fc
Anton Zarembo <anton.zarembo@onilab.com>
2022-01-11 19:09:21 +0300

    feat(root): remove store provider

--- src/actility/src/AppModule.tsx
+++ src/actility/src/AppModule.tsx
@@ -1,14 +1,17 @@
-import { module } from '@pwa-concept/core'
+import { module, PROVIDE_GRAPHQL_POLICY_TOKEN } from '@pwa-concept/core'
 import { BrowserRouter } from 'react-router-dom'
-import { CurrencyProvider, MediaBreakpointProvider, StoreProvider } from 'providers'
+import { CurrencyProvider, MediaBreakpointProvider } from 'providers'
+import { createTypePolicies } from 'providers/StoreProvider/graphql/createTypePolicies'
 import App from './App'
 
 export const AppModule = module(() => ({
 	entry: <App />,
+	provides: [
+		{ use: PROVIDE_GRAPHQL_POLICY_TOKEN, value: createTypePolicies() },
+	],
 	providers: [
 		BrowserRouter,
 		MediaBreakpointProvider,
-		StoreProvider,
 		CurrencyProvider,
 	],
 }))

47d8bf62a8cffc8a5523ef7719af990481425dc3
Anton Zarembo <anton.zarembo@onilab.com>
2022-01-31 15:35:17 +0300

    feat(core): add route and route fallback hooks

--- packages/core/router/index.ts
+++ packages/core/router/index.ts
@@ -1,2 +1,4 @@
 export { default as RouterProvider } from './Provider'
+export { default as useRoute } from './Route'
+export { default as useRouteFallback } from './useRouteFallback'
 export * from './tokens'

29410b18a6262e7e5c4ace1e2f1e16b1fa0a20b0
Anton Zarembo <anton.zarembo@onilab.com>
2022-01-31 16:01:49 +0300

    feat(core): added listeners hooks

--- packages/core/hooks/index.ts
+++ packages/core/hooks/index.ts
@@ -1,1 +1,4 @@
 export { default as useInitEffect } from './useInitEffect'
+export { default as useOnDocumentListener } from './useOnDocumentListener'
+export { default as useOnListener } from './useOnListener'
+export { default as useOnWindowListener } from './useOnWindowListener'

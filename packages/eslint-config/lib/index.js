3e457865705e1bffa851b3f53c11a3d552f86f83
Anton Zarembo <anton.zarembo@onilab.com>
2022-02-01 09:25:23 +0300

    feat(eslint-config): update rules

---
+++ packages/eslint-config/lib/index.js
@@ -0,0 +1,38 @@
+const rules = require('./rules')
+const overrides = require('./overrides')
+
+module.exports = {
+	root: true,
+	parserOptions: {
+		ecmaVersion: 2021,
+		ecmaFeatures: {
+			jsx: true,
+		},
+		sourceType: 'module',
+	},
+	env: {
+		es2021: true,
+		node: true,
+	},
+	plugins: [
+		'import',
+		'promise',
+		'react',
+		'@typescript-eslint',
+	],
+	settings: {
+		react: {
+			version: '17',
+		},
+		linkComponents: [
+			'Link',
+		],
+	},
+	globals: {
+		document: 'readonly',
+		navigator: 'readonly',
+		window: 'readonly',
+	},
+	rules,
+	overrides,
+}

3fd99dd3bb80d332a823fad2653ed0b590a17c06
Anton Zarembo <anton.zarembo@onilab.com>
2022-02-15 16:19:43 +0300

    feat(modules): added pagination fragment

---
+++ packages/modules/pagination/graphql/fragments/Pagination.tsx
@@ -0,0 +1,22 @@
+/**
+ * NOTE: THIS IS AN AUTO-GENERATED FILE. DO NOT MODIFY IT DIRECTLY.
+ * USE `yarn build:schema-types` or `yarn build`.
+ */
+
+/* eslint-disable */
+/* tslint:disable */
+/* @ts-nocheck */
+
+import * as Types from '../../../graphql';
+
+import { gql } from '@apollo/client';
+export type PaginationFragment = { __typename?: 'Pagination', count?: number | null | undefined, limit?: number | null | undefined, total?: number | null | undefined, current?: number | null | undefined };
+
+export const PaginationFragmentDoc = gql`
+    fragment Pagination on Pagination {
+  count
+  limit
+  total
+  current
+}
+    `;

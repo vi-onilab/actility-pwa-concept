065761e17d41d7fe74c604d33edc8febd79bef0c
Anton Zarembo <anton.zarembo@onilab.com>
2022-02-23 11:39:10 +0300

    fix(modules): add product vat into product

--- packages/modules/product/graphql/fragments/Product.tsx
+++ packages/modules/product/graphql/fragments/Product.tsx
@@ -10,9 +10,10 @@
 import * as Types from '../../../graphql';
 
 import { gql } from '@apollo/client';
+import { ProductVatFragmentDoc } from './ProductVat';
 import { ProductImageFragmentDoc } from './ProductImage';
 import { ProductPriceFragmentDoc } from './ProductPrice';
-export type ProductFragment = { __typename?: 'Product', id?: string | null | undefined, name?: string | null | undefined, breadcrumbs?: Array<{ __typename?: 'ProductBreadcrumb', id?: string | null | undefined, name?: string | null | undefined } | null | undefined> | null | undefined, manufacturer?: { __typename?: 'ProductManufacturer', name?: string | null | undefined } | null | undefined, stock?: { __typename?: 'ProductStock', name?: string | null | undefined, type?: Types.ProductStockType | null | undefined } | null | undefined, badges?: Array<{ __typename?: 'ProductBadge', id?: string | null | undefined, name?: string | null | undefined, icon?: string | null | undefined } | null | undefined> | null | undefined, images?: Array<{ __typename?: 'ProductImage', id?: string | null | undefined, description?: string | null | undefined, url?: string | null | undefined } | null | undefined> | null | undefined, thumbnail?: { __typename?: 'ProductImage', id?: string | null | undefined, description?: string | null | undefined, url?: string | null | undefined } | null | undefined, price?: { __typename?: 'ProductPrice', isRange?: boolean | null | undefined, current?: { __typename?: 'Money', value?: number | null | undefined, text?: string | null | undefined, currency?: string | null | undefined } | null | undefined, initial?: { __typename?: 'Money', value?: number | null | undefined, text?: string | null | undefined, currency?: string | null | undefined } | null | undefined } | null | undefined };
+export type ProductFragment = { __typename?: 'Product', id?: string | null | undefined, name?: string | null | undefined, breadcrumbs?: Array<{ __typename?: 'ProductBreadcrumb', id?: string | null | undefined, name?: string | null | undefined } | null | undefined> | null | undefined, manufacturer?: { __typename?: 'ProductManufacturer', name?: string | null | undefined } | null | undefined, stock?: { __typename?: 'ProductStock', name?: string | null | undefined, type?: Types.ProductStockType | null | undefined } | null | undefined, badges?: Array<{ __typename?: 'ProductBadge', id?: string | null | undefined, name?: string | null | undefined, icon?: string | null | undefined } | null | undefined> | null | undefined, vat?: { __typename?: 'ProductVat', type?: Types.ProductVatType | null | undefined, name?: string | null | undefined } | null | undefined, images?: Array<{ __typename?: 'ProductImage', id?: string | null | undefined, description?: string | null | undefined, url?: string | null | undefined } | null | undefined> | null | undefined, thumbnail?: { __typename?: 'ProductImage', id?: string | null | undefined, description?: string | null | undefined, url?: string | null | undefined } | null | undefined, price?: { __typename?: 'ProductPrice', isRange?: boolean | null | undefined, current?: { __typename?: 'Money', value?: number | null | undefined, text?: string | null | undefined, currency?: string | null | undefined } | null | undefined, initial?: { __typename?: 'Money', value?: number | null | undefined, text?: string | null | undefined, currency?: string | null | undefined } | null | undefined } | null | undefined };
 
 export const ProductFragmentDoc = gql`
     fragment Product on Product {
@@ -34,6 +35,9 @@ export const ProductFragmentDoc = gql`
     name
     icon
   }
+  vat {
+    ...ProductVat
+  }
   images {
     ...ProductImage
   }
@@ -44,5 +48,6 @@ export const ProductFragmentDoc = gql`
     ...ProductPrice
   }
 }
-    ${ProductImageFragmentDoc}
+    ${ProductVatFragmentDoc}
+${ProductImageFragmentDoc}
 ${ProductPriceFragmentDoc}`;
---
+++ packages/modules/product/graphql/fragments/ProductVat.tsx
@@ -0,0 +1,20 @@
+/**
+ * NOTE: THIS IS AN AUTO-GENERATED FILE. DO NOT MODIFY IT DIRECTLY.
+ * USE `yarn build:schema-types` or `yarn build`.
+ */
+
+/* eslint-disable */
+/* tslint:disable */
+/* @ts-nocheck */
+
+import * as Types from '../../../graphql';
+
+import { gql } from '@apollo/client';
+export type ProductVatFragment = { __typename?: 'ProductVat', type?: Types.ProductVatType | null | undefined, name?: string | null | undefined };
+
+export const ProductVatFragmentDoc = gql`
+    fragment ProductVat on ProductVat {
+  type
+  name
+}
+    `;

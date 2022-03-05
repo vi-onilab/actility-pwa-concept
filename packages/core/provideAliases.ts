31d0d29dddf38c2ea121d5859e82be2f952a87c3
Anton Zarembo <anton.zarembo@onilab.com>
2022-03-05 13:03:48 +0300

    fix(core): provide aliases for fragments

--- packages/core/provideAliases.ts
+++ packages/core/provideAliases.ts
@@ -6,6 +6,7 @@ import {
     PROVIDE_GRAPHQL_RESOLVERS,
     PROVIDE_GRAPHQL_STORE_FRAGMENTS,
     PROVIDE_GRAPHQL_STORE_POSSIBLE_TYPES,
+    PROVIDE_GRAPHQL_FRAGMENTS,
 } from './graphql'
 import { ModuleProvideAliases, ProvideToken } from './types'
 
@@ -15,6 +16,7 @@ const provideAliases: Record<keyof ModuleProvideAliases, ProvideToken> = {
     graphqlLinks: PROVIDE_GRAPHQL_LINKS,
     graphqlSchemas: PROVIDE_GRAPHQL_SCHEMAS,
     graphqlResolvers: PROVIDE_GRAPHQL_RESOLVERS,
+    graphqlFragments: PROVIDE_GRAPHQL_FRAGMENTS,
     graphqlStoreFragments: PROVIDE_GRAPHQL_STORE_FRAGMENTS,
     graphqlStorePossibleTypes: PROVIDE_GRAPHQL_STORE_POSSIBLE_TYPES,
 }

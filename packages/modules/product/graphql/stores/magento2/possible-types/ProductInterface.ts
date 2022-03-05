5530b33edd4f9a2c07adc03e0b6547ea881f5eb8
Anton Zarembo <anton.zarembo@onilab.com>
2022-03-05 13:05:22 +0300

    fix(modules): failed build because of codegen

--- packages/modules/product/graphql/stores/magento2/possible-types/ProductInterface.ts
+++ packages/modules/product/graphql/stores/magento2/possible-types/ProductInterface.ts
@@ -1,4 +1,4 @@
-import { StorePossibleTypes } from '~core'
+import { StorePossibleTypes } from '@pwa-concept/core'
 import { Magento2StorePossibleTypesKeys } from '@pwa-concept/stores/magento2/graphql'
 
 const ProductInterface: StorePossibleTypes<Magento2StorePossibleTypesKeys>['ProductInterface'] = [

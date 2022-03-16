2a35013fd2962a90dd2e59b4e20072ebbdf890c4
Anton Zarembo <anton.zarembo@onilab.com>
2022-03-16 14:55:28 +0300

    fix(core): graphql Provider spaces

--- packages/core/graphql/Provider.tsx
+++ packages/core/graphql/Provider.tsx
@@ -12,7 +12,7 @@ const GraphQLProvider: FC = ({ children }) => {
     const typeDefs = useProvide<TypeSource[], any>(PROVIDE_GRAPHQL_SCHEMAS, null, (value) => (
         value?.length > 0 ? value : undefined
     ))
-    
+
     const resolvers = useProvide<any>(PROVIDE_GRAPHQL_RESOLVERS, [], (value) => {
         if (!value?.length) return undefined
 

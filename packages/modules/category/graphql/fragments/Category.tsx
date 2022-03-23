8ec94890c2a9e1ebde2a154381e590ae32e7c1e2
Anton Zarembo <anton.zarembo@onilab.com>
2022-03-23 13:36:14 +0300

    feat: remove custom fields from category module

--- packages/modules/category/graphql/fragments/Category.tsx
+++ packages/modules/category/graphql/fragments/Category.tsx
@@ -10,7 +10,7 @@ import { gql } from '@apollo/client';
 /* tslint:disable */
 // @ts-nocheck
 
-export type CategoryFragment = { __typename?: 'Category', id?: string | null | undefined, name?: string | null | undefined, description?: string | null | undefined, level?: number | null | undefined, icon?: string | null | undefined, banners?: Array<{ __typename?: 'CategoryBanner', id?: string | null | undefined, title?: string | null | undefined, description?: string | null | undefined, image?: { __typename?: 'CategoryBannerImage', id?: string | null | undefined, url?: string | null | undefined, sources?: Array<{ __typename?: 'CategoryBannerImageSource', id?: string | null | undefined, url?: string | null | undefined, media?: string | null | undefined } | null | undefined> | null | undefined } | null | undefined } | null | undefined> | null | undefined, breadcrumbs?: Array<{ __typename?: 'CategoryBreadcrumb', id?: string | null | undefined, name?: string | null | undefined, level?: number | null | undefined } | null | undefined> | null | undefined, images?: { __typename?: 'CategoryImages', thumbnail?: { __typename?: 'CategoryImage', id?: string | null | undefined, url?: string | null | undefined } | null | undefined, main?: { __typename?: 'CategoryImage', id?: string | null | undefined, url?: string | null | undefined } | null | undefined } | null | undefined, parent?: { __typename?: 'Category', id?: string | null | undefined, name?: string | null | undefined, description?: string | null | undefined, level?: number | null | undefined } | null | undefined };
+export type CategoryFragment = { __typename?: 'Category', id?: string | null | undefined, name?: string | null | undefined, description?: string | null | undefined, level?: number | null | undefined, breadcrumbs?: Array<{ __typename?: 'CategoryBreadcrumb', id?: string | null | undefined, name?: string | null | undefined, level?: number | null | undefined } | null | undefined> | null | undefined, parent?: { __typename?: 'Category', id?: string | null | undefined, name?: string | null | undefined, description?: string | null | undefined, level?: number | null | undefined } | null | undefined };
 
 export const CategoryFragmentDoc = gql`
     fragment Category on Category {
@@ -18,35 +18,10 @@ export const CategoryFragmentDoc = gql`
   name
   description
   level
-  icon
-  banners {
-    id
-    title
-    description
-    image {
-      id
-      url
-      sources {
-        id
-        url
-        media
-      }
-    }
-  }
   breadcrumbs {
     id
     name
     level
-  }
-  images {
-    thumbnail {
-      id
-      url
-    }
-    main {
-      id
-      url
-    }
   }
   parent {
     id
--- packages/modules/category/graphql/stores/magento2/resolvers/Category.ts
+++ packages/modules/category/graphql/stores/magento2/resolvers/Category.ts
@@ -6,46 +6,11 @@ const Category: Resolvers['Category'] = {
     id: (_, __, { context }) => id(context),
     name: (_, __, { context }) => context?.name,
     description: (_, __, { context }) => context?.description,
-    icon: (_, __, { context }) => context?.category_widget_svg_icon,
     level: (_, __, { context }) => context?.level ? Number(context?.level) : 0,
     parent: (_, __, { context }) => context?.parent_category ? ({
         __context: context?.parent_category,
         __typename: 'Category',
     }) : null,
-    images: (_, __, { context }) => ({
-        thumbnail: context?.image_url ? {
-            id: `${id(context)}_thumbnail`,
-            url: context.image_url,
-            __typename: 'CategoryImage',
-        } : null,
-        main: (context?.category_widget_thumbnail || context?.image_url) ? {
-            id: `${id(context)}_main`,
-            url: context?.category_widget_thumbnail || context?.image_url,
-            __typename: 'CategoryImage',
-        } : null,
-        __typename: 'CategoryImages',
-    }),
-    banners: (_, __, { context }) => ([
-        {
-            id: id(context),
-            title: context?.name,
-            description: context?.description,
-            image: {
-                id: `${id(context)}_0`,
-                url: context?.top_banner,
-                sources: [
-                    {
-                        id: `${id(context)}_0`,
-                        url: context?.top_banner_mobile,
-                        media: '(max-width: 768px)',
-                        __typename: 'CategoryBannerImageSource',
-                    },
-                ],
-                __typename: 'CategoryBannerImage',
-            },
-            __typename: 'CategoryBanner',
-        },
-    ]),
     breadcrumbs: (_, __, { context }) => ([
         ...(context?.breadcrumbs || [])?.map?.((breadcrumb) => ({
             id: !breadcrumb?.category_id ? null : String(breadcrumb?.category_id),

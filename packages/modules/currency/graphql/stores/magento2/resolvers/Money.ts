ed3028d3e73278f073b135521a82fba52d73c5fa
Anton Zarembo <anton.zarembo@onilab.com>
2022-03-23 14:28:25 +0300

    fix(modules): missing fields in currency module

--- packages/modules/currency/graphql/stores/magento2/resolvers/Money.ts
+++ packages/modules/currency/graphql/stores/magento2/resolvers/Money.ts
@@ -2,7 +2,9 @@ import { Resolvers } from '@pwa-concept/modules/graphql'
 import { convertMoneyToText } from '@pwa-concept/modules/currency/utils'
 
 const Money: Resolvers['Money'] = {
+    currency: ({ currency } = {}) => currency || null,
     text: async (money) => convertMoneyToText(money),
+    value: ({ value } = {}) => value || 0,
 }
 
 export default Money

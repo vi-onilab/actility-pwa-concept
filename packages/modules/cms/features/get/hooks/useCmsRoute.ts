86d0fba4d7882ece77edd13a9c847456cc242914
Anton Zarembo <anton.zarembo@onilab.com>
2022-04-12 11:43:19 +0300

    feat(modules): add local state to cms route

--- packages/modules/cms/features/get/hooks/useCmsRoute.ts
+++ packages/modules/cms/features/get/hooks/useCmsRoute.ts
@@ -2,7 +2,7 @@ import { useLocation } from 'react-router-dom'
 import { useCmsRouteQuery } from '@pwa-concept/modules/cms/graphql/queries/CmsRoute'
 
 const useCmsRoute = () => {
-    const { pathname } = useLocation()
+    const { pathname, state = null } = useLocation()
     const { data, loading } = useCmsRouteQuery({
         variables: {
             url: pathname,
@@ -10,7 +10,7 @@ const useCmsRoute = () => {
     })
 
     return {
-        data: data?.cmsRoute,
+        data: data?.cmsRoute ?? (state as any)?.cms,
         loading,
     }
 }

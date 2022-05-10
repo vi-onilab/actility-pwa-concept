06f4545f94e361d0223d8b14f83529e962ee3f75
Anton Zarembo <anton.zarembo@onilab.com>
2022-05-10 12:41:54 +0300

    fix(modules): sort resolver

--- packages/modules/sort/graphql/stores/magento2/resolvers/index.ts
+++ packages/modules/sort/graphql/stores/magento2/resolvers/index.ts
@@ -1,9 +1,11 @@
 import { Resolvers } from '@pwa-concept/modules/graphql'
 import Query from './Query'
+import Sort from './Sort'
 import SortsQueryOutput from './SortsQueryOutput'
 
 const resolvers: Resolvers = {
     Query,
+    Sort,
     SortsQueryOutput,
 }
 

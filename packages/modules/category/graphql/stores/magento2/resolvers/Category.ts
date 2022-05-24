8618b96e30d19084401acd37927fc19899e1da42
Anton Zarembo <anton.zarembo@onilab.com>
2022-05-24 13:32:41 +0300

    fix(modules): category breadcrumbs

--- packages/modules/category/graphql/stores/magento2/resolvers/Category.ts
+++ packages/modules/category/graphql/stores/magento2/resolvers/Category.ts
@@ -1,4 +1,4 @@
-import { Resolvers } from '@pwa-concept/modules/graphql'
+import { Resolvers, CategoryBreadcrumbUrlType } from '@pwa-concept/modules/graphql'
 
 const id = (context) => String(context.id)
 
@@ -16,12 +16,19 @@ const Category: Resolvers['Category'] = {
             id: !breadcrumb?.category_id ? null : String(breadcrumb?.category_id),
             name: breadcrumb?.category_name,
             level: +breadcrumb?.category_level || null,
+            url: {
+                id: String(breadcrumb?.category_id),
+                type: CategoryBreadcrumbUrlType.Category,
+                to: breadcrumb?.category_url_path?.length > 0 ? `/${breadcrumb.category_url_path}` : null,
+                __typename: 'CategoryBreadcrumbUrl',
+            },
             __typename: 'CategoryBreadcrumb',
         })),
         {
             id: id(context),
             level: 0,
             name: context?.name,
+            url: null,
             __typename: 'CategoryBreadcrumb',
         },
     ]),

dafb28915542bfad5b20a5a8b24e101029b5327d
Anton Zarembo <anton.zarembo@onilab.com>
2022-06-10 08:21:40 +0300

    fix(modules): 'search' or 'filter' input argument is required

--- packages/modules/sort/graphql/stores/magento2/resolvers/Query/sorts.ts
+++ packages/modules/sort/graphql/stores/magento2/resolvers/Query/sorts.ts
@@ -3,11 +3,18 @@ import { gql } from 'graphql-tag'
 import { QueryResolvers } from '@pwa-concept/modules/graphql'
 
 const sorts: QueryResolvers['sorts'] = async () => {
+    // TODO: hack with filter need for fix "'search' or 'filter' input argument is required." (filter: { price: { from: "0" } })
     const { data: { products: data = {} } = {} } = await (
         api.graphql(
             gql`
                 query {
-                    products {
+                    products(
+                        filter: {
+                            price: {
+                                from: "0"
+                            }
+                        }
+                    ) {
                         sort_fields {
                             ... SortFields
                         }

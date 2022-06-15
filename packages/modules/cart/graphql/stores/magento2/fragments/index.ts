668d1aa1e9a2de7fad66079e5fba0b7b2f36f2b8
Anton Zarembo <anton.zarembo@onilab.com>
2022-06-15 11:01:16 +0300

    feat(modules): improve cart magento 2 fragments

--- packages/modules/cart/graphql/stores/magento2/fragments/index.ts
+++ packages/modules/cart/graphql/stores/magento2/fragments/index.ts
@@ -1,8 +1,21 @@
 import { StoreFragments } from '@pwa-concept/core'
+
+import BillingCartAddress from './BillingCartAddress.graphql'
 import Cart from './Cart.graphql'
+import CartAddressInterface from './CartAddressInterface.graphql'
+import CartItemInterface from './CartItemInterface.graphql'
+import CartPrices from './CartPrices.graphql'
+import SelectedCustomizableOption from './SelectedCustomizableOption.graphql'
+import ShippingCartAddress from './ShippingCartAddress.graphql'
 
 const fragments: StoreFragments = [
+    BillingCartAddress,
     Cart,
+    CartAddressInterface,
+    CartItemInterface,
+    CartPrices,
+    SelectedCustomizableOption,
+    ShippingCartAddress,
 ]
 
 export default fragments
--- packages/modules/cart/graphql/stores/magento2/possible-types/index.ts
+++ packages/modules/cart/graphql/stores/magento2/possible-types/index.ts
@@ -1,6 +1,16 @@
 import { StorePossibleTypes } from '@pwa-concept/core'
 import { Magento2StorePossibleTypesKeys } from '@pwa-concept/stores/magento2/graphql'
 
-const possibleTypes: StorePossibleTypes<Magento2StorePossibleTypesKeys> = {}
+const possibleTypes: StorePossibleTypes<Magento2StorePossibleTypesKeys> = {
+    CartAddressInterface: ['ShippingCartAddress', 'BillingCartAddress'],
+    CartItemInterface: [
+        'BundleCartItem',
+        'ConfigurableCartItem',
+        'DownloadableCartItem',
+        'GiftCardCartItem',
+        'SimpleCartItem',
+        'VirtualCartItem',
+    ],
+}
 
 export default possibleTypes

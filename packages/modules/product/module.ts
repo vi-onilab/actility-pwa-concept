7e861b344b9ef3199849e70342bb3071ec746bd1
Anton Zarembo <anton.zarembo@onilab.com>
2022-06-21 15:28:23 +0300

    feat(modules): add support for product partial data

--- packages/modules/product/module.ts
+++ packages/modules/product/module.ts
@@ -1,6 +1,7 @@
 import { module } from '@pwa-concept/core'
 import { CurrencyModule } from '@pwa-concept/modules/currency'
 import { PaginationModule } from '@pwa-concept/modules/pagination'
+
 import graphqlSchemas from './graphql/schemas'
 import stores from './graphql/stores'
 
@@ -12,15 +13,22 @@ const ProductModule = module(() => ({
     graphqlSchemas,
     graphqlPolicies: {
         Query: {
+            keyFields: (product) => `${product.__typename}:${product.id}`,
             fields: {
                 product: {
-                    read(existing, { args, toReference }) {
-                        return existing ?? toReference({
-                            id: args?.id,
-                            sku: args?.sku,
-                            url: args?.url,
-                            __typename: 'Product',
-                        })
+                    read(existing, { args, toReference, canRead }) {
+                        if (!existing) {
+                            const reference = toReference({
+                                id: args?.input?.id,
+                                sku: args?.input?.sku,
+                                url: args?.input?.url,
+                                __typename: 'Product',
+                            })
+
+                            if (canRead(reference)) return reference
+                        }
+
+                        return existing
                     },
                 },
             },

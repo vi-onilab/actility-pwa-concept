a63d153ed917996cd44e6032f8b2d22cfd1a3cf1
Anton Zarembo <anton.zarembo@onilab.com>
2022-06-21 11:27:34 +0300

    feat(modules): add error policy option to useCart hook

--- packages/modules/cart/features/get/hooks/useCart.ts
+++ packages/modules/cart/features/get/hooks/useCart.ts
@@ -1,13 +1,22 @@
-import { useCartQuery } from '@pwa-concept/modules/cart/graphql/queries/Cart'
+import { ErrorPolicy } from '@apollo/client'
 import { $cart } from '@pwa-concept/core/models'
+import { CartQueryVariables, useCartQuery } from '@pwa-concept/modules/cart/graphql/queries/Cart'
 
-const useCart = ({ isFull = false }: { isFull?: boolean }) => {
+interface UseCartOptions {
+    errorPolicy: ErrorPolicy
+}
+
+interface UseCartVariables extends Pick<CartQueryVariables, 'isFull'> {
+}
+
+const useCart = (input: Partial<UseCartVariables> = {}, options: Partial<UseCartOptions> = {}) => {
     const [cartId] = $cart.useId()
 
     const { data: { cart = {} } = {}, loading } = useCartQuery({
+        ...options,
         variables: {
+            ...input,
             cartId,
-            isFull,
         },
     })
 

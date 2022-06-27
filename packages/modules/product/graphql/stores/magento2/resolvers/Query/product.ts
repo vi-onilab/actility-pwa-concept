7f2ce1ab5f5d75ee7f5ee3d33880bc0afd00c5aa
Anton Zarembo <anton.zarembo@onilab.com>
2022-06-27 14:51:56 +0300

    feat(modules): merge configurble and simple product in m2 product query resolver

--- packages/modules/product/graphql/stores/magento2/resolvers/Query/product.ts
+++ packages/modules/product/graphql/stores/magento2/resolvers/Query/product.ts
@@ -2,6 +2,28 @@ import { gql } from '@apollo/client'
 import api from '@pwa-concept/core/api'
 import { QueryResolvers } from '@pwa-concept/modules/graphql'
 
+const smartMerge = (target, source) => {
+    const softMerge = { ...target, ...source }
+
+    return (
+        Object.keys(source).reduce((result, field) => {
+            const value = softMerge?.[field]
+
+            if (
+                value === null ||
+                value === undefined ||
+                (typeof value === 'string' && !value?.length) ||
+                (Array.isArray(value) && !value?.length) ||
+                (typeof value === 'object' && !Object.keys(value)?.length)
+            ) {
+                result[field] = target?.[field]
+            }
+
+            return result
+        }, softMerge)
+    )
+}
+
 const product: QueryResolvers['product'] = async (_, { input }) => {
     const {
         id = null,
@@ -61,6 +83,25 @@ const product: QueryResolvers['product'] = async (_, { input }) => {
     )
 
     if (!products?.items?.length) return null
+
+    if (products?.length === 2) {
+        const defaultValue = { configurable: null, simple: null }
+
+        const { configurable, simple } = products?.items?.reduce((result, item) => {
+            if (item?.__typename === 'SimpleProduct') {
+                result.simple = item
+            } else {
+                result.configurable = item
+            }
+
+            return result
+        }, defaultValue) || defaultValue
+
+        return {
+            __context: smartMerge(configurable, simple),
+            __typename: 'Product',
+        }
+    }
 
     return {
         __context: products?.items?.[0],

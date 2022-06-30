24d022672be560493b6b89d11d5c3b7ec0f59d58
Anton Zarembo <anton.zarembo@onilab.com>
2022-06-30 15:08:25 +0300

    feat(modules): add sort filters by position

--- packages/modules/filter/graphql/stores/magento2/resolvers/Query/filters.ts
+++ packages/modules/filter/graphql/stores/magento2/resolvers/Query/filters.ts
@@ -1,5 +1,5 @@
-import { QueryResolvers } from '@pwa-concept/modules/graphql'
 import api from '@pwa-concept/core/api'
+import { QueryResolvers } from '@pwa-concept/modules/graphql'
 import { gql } from 'graphql-tag'
 
 const filters: QueryResolvers['filters'] = async (_, { input }) => {
@@ -30,10 +30,12 @@ const filters: QueryResolvers['filters'] = async (_, { input }) => {
         ).query()
     )
 
-    return products?.aggregations?.map?.((__context) => ({
-        __context,
-        __typename: 'Filter',
-    }))
+    return [...products?.aggregations || []]
+        ?.sort?.((a, b) => +a?.position > +b?.position ? 1 : -1)
+        ?.map?.((__context) => ({
+            __context,
+            __typename: 'Filter',
+        }))
 }
 
 export default filters

f4b472ec45763d6633cb63d1b7889b8aa74bec3f
Anton Zarembo <anton.zarembo@onilab.com>
2022-07-13 14:46:50 +0300

    feat(core): add PROVIDE_GRAPHQL_AUTHORIZATION_INVALID_TOKEN_EXCLUDE_PATHS for graphql

--- packages/core/graphql/links/authError.ts
+++ packages/core/graphql/links/authError.ts
@@ -1,15 +1,19 @@
-import { onError } from '@apollo/client/link/error'
-import { $auth, $clean } from '@pwa-concept/core/models'
+import { onError } from '@apollo/client/link/error';
+import { Provide, PROVIDE_GRAPHQL_AUTHORIZATION_INVALID_TOKEN_EXCLUDE_PATHS } from '@pwa-concept/core';
+import { $auth, $clean } from '@pwa-concept/core/models';
 
 const authErrorLink = (
     onError(({ graphQLErrors }) => {
         if (graphQLErrors) {
+            const pathsFromTheme = Provide.first(PROVIDE_GRAPHQL_AUTHORIZATION_INVALID_TOKEN_EXCLUDE_PATHS) || []
+
             const ignorePaths = [
                 'cart',
                 'compare',
                 'wishlist',
                 'assignCompareListToCustomer',
                 'mergeCarts',
+                ...pathsFromTheme,
             ]
             const authError = graphQLErrors.find(({ extensions, path }) => (
                 extensions?.category === 'graphql-authorization' &&

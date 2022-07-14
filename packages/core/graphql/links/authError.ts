a7634b9052f9abcfa25a77c2a0148ca8231885a9
Anton Zarembo <anton.zarembo@onilab.com>
2022-07-14 09:27:03 +0300

    feat(core): improve authErrorLink and make it extendable

--- packages/core/graphql/links/authError.ts
+++ packages/core/graphql/links/authError.ts
@@ -1,30 +1,54 @@
-import { onError } from '@apollo/client/link/error';
-import { Provide, PROVIDE_GRAPHQL_AUTHORIZATION_INVALID_TOKEN_EXCLUDE_PATHS } from '@pwa-concept/core';
-import { $auth, $clean } from '@pwa-concept/core/models';
+import { onError } from "@apollo/client/link/error";
+import {
+    Provide,
+    PROVIDE_GRAPHQL_CUSTOMER_OPERATIONS_GUARD,
+    PROVIDE_GRAPHQL_CUSTOMER_OPERATIONS_GUARD_MIDDLEWARE
+} from "@pwa-concept/core";
+import { $auth, $clean } from "@pwa-concept/core/models";
 
-const authErrorLink = (
-    onError(({ graphQLErrors }) => {
-        if (graphQLErrors) {
-            const pathsFromTheme = Provide.first(PROVIDE_GRAPHQL_AUTHORIZATION_INVALID_TOKEN_EXCLUDE_PATHS) || []
+const authErrorLink = onError((error) => {
+    const { graphQLErrors } = error
 
-            const ignorePaths = [
-                'cart',
-                'compare',
-                'wishlist',
-                'assignCompareListToCustomer',
-                'mergeCarts',
+    if (graphQLErrors) {
+        const middlewareFn = Provide.first(PROVIDE_GRAPHQL_CUSTOMER_OPERATIONS_GUARD_MIDDLEWARE) || [];
+
+        const next = () => {
+            const pathsFromTheme = Provide.first(PROVIDE_GRAPHQL_CUSTOMER_OPERATIONS_GUARD) || [];
+
+            const invalidTokenPaths = [
+                'customer',
+                'updateCustomerAddress',
+                'createCustomerAddress',
+                'deleteCustomerAddress',
+                'changeCustomerPassword',
+                'createWishlist',
+                'deleteWishlist',
+                'updateProductsInWishlist',
+                'updateWishlist',
+                'revokeCustomerToken',
+                'updateCustomer',
+                'updateCustomerEmail',
+                'updateCustomerV2',
                 ...pathsFromTheme,
             ]
-            const authError = graphQLErrors.find(({ extensions, path }) => (
-                extensions?.category === 'graphql-authorization' &&
-                !path.find((location: string) => ignorePaths.includes(location))
-            ))
+
+            const authError = graphQLErrors.find(
+                ({ extensions, path }) => (
+                    extensions?.category === "graphql-authorization" && path.find((location: string) => invalidTokenPaths.includes(location))
+                )
+            )
 
             if (authError && $auth.isToken) {
                 $clean.logout()
             }
         }
-    })
-)
+
+        if (middlewareFn) {
+            middlewareFn?.(error, next)
+        } else {
+            next()
+        }
+    }
+})
 
 export default authErrorLink
